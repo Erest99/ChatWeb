d64b687fa0266c9e4354c954df314896621df30f
Matyas.Horky <62426727+Erest99@users.noreply.github.com>
2022-12-08 08:50:39 +0100

    Add files via upload
    rozpracováno

---
+++ ChatWeb/chat.php
@@ -0,0 +1,55 @@
+<?php include_once "header.php";?>
+    <body>
+        <div class = "wrapper">
+            <section class ="chat-area">
+                <header>
+                    <a href="#" class="back-icon" ><i class="fas fa-arrow-left"></i></a>
+                    <img src="pika.jpg" alt="">
+                    <div class="details">
+                        <span>Maty</span>
+                        <p>Now active</p>
+                    </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="pika.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus maxime, tempore totam sapiente repudiandae sequi sed dicta libero aspernatur vero dolores et quae sint fugit ducimus, cupiditate unde, fugiat ad!</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="pika.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus maxime, tempore totam sapiente repudiandae sequi sed dicta libero aspernatur vero dolores et quae sint fugit ducimus, cupiditate unde, fugiat ad!</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="pika.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus maxime, tempore totam sapiente repudiandae sequi sed dicta libero aspernatur vero dolores et quae sint fugit ducimus, cupiditate unde, fugiat ad!</p>
+                    </div>
+                </div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" placeholder="Type a message">
+                <button><i class="fa fa-arrow-circle-up"></i></button>
+            </form>
+            </section>
+        </div>
+    </body>
+</html>
---
+++ ChatWeb/users.php
@@ -0,0 +1,103 @@
+    <?php 
+        session_start();
+        if(!isset($_SESSION['unique_id']))
+        {
+            header("location: login.php");
+        }
+    ?>
+    <?php include_once "header.php";?>
+    <body>
+        <div class = "wrapper">
+            <section class ="users">
+                <header>
+                    <?php 
+                    include_once "php/config.php";
+                        $sql = mysqli_query($conn,"SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                        if(mysqli_num_rows($sql)>0)
+                        {
+                            $row = mysqli_fetch_assoc($sql);
+                        }
+                    ?>
+                    <div class="content">
+                        <img src="php/images/<?php echo $row['img']  ?>" alt="">
+                        <div class="details">
+                            <span><?php echo $row['fname']." ".$row['lname'] ?></span>
+                            <p><?php echo $row['status']  ?></p>
+                        </div>
+                    </div>
+                    <a href="#" class="logout">Logout</a>
+                </header>
+                <div class="search">
+                    <span class="text">Select user to start conversation</span>
+                    <input type="text" placeholder="Search">
+                    <button><i class="fas fa-search"></i></button>
+                </div>
+                <div class="users-list">
+                    <a href="#">
+                        <div class="content">
+                            <img src="#" alt="">
+                            <div class="details">
+                            <span>Mints</span>
+                            <p>test status</p>
+                            </div>
+                        </div>
+                        <div class="status-dot">
+                            <i class="fas fa-circle">
+
+                            </i>
+                        </div>
+                    </a>
+                </div>
+                <div class="users-list">
+                    <a href="#">
+                        <div class="content">
+                            <img src="#" alt="">
+                            <div class="details">
+                            <span>Mints</span>
+                            <p>test status</p>
+                            </div>
+                        </div>
+                        <div class="status-dot">
+                            <i class="fas fa-circle">
+                                
+                            </i>
+                        </div>
+                    </a>
+                </div>
+                <div class="users-list">
+                    <a href="#">
+                        <div class="content">
+                            <img src="#" alt="">
+                            <div class="details">
+                            <span>Mints</span>
+                            <p>test status</p>
+                            </div>
+                        </div>
+                        <div class="status-dot">
+                            <i class="fas fa-circle">
+                                
+                            </i>
+                        </div>
+                    </a>
+                </div>
+                <div class="users-list">
+                    <a href="#">
+                        <div class="content">
+                            <img src="#" alt="">
+                            <div class="details">
+                            <span>Mints</span>
+                            <p>test status</p>
+                            </div>
+                        </div>
+                        <div class="status-dot">
+                            <i class="fas fa-circle">
+                                
+                            </i>
+                        </div>
+                    </a>
+                </div>
+            </section>
+        </div>
+        <script src="javascript/users.js"></script>
+    </body>
+</html>
